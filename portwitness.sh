c0b08c7d48a5f4eda10ab696e9621fbd1bdae54b
smartengineers <pig.wig45@gmail.com>
2018-04-14 06:16:28 +0530

    changes in the if section of code

--- portwitness.sh
+++ portwitness.sh
@@ -11,29 +11,26 @@ else
         echo -e  "\033[34m ###                OSINT @tikoo_sahil ###\n"
 	echo "[*] Want to check for the sub-domain enum(Y,N)?"
         read enum
-        if [ "$enum" == "Y" ]; then
+        if [ "$enum" == "Y" ] || [ "$enum" == "y" ]; then
 	       cd Sublist3r
                echo "Searching For Sub-Domains ........"
-               `./sublist3r.py -d $1 -o output.txt`
-               echo output.txt > x
+               m=`./sublist3r.py -d $1 -o output.txt`
                while read -r line; do
-                        echo `nslookup $line | awk '/ Address: / { print $2 }'` > ip.txt
-               done < "$x"
+                        echo `nslookup $line | awk '/^Address: / { print $2 }'` >> ip.txt
+               done < "output.txt"
                echo "[*] IP's for all sub-domains generated in ./ip.txt"
-               echo ` nmap -p 80,443 -iL -Pn  ip.txt` >  port_status.txt
-               Working_ips= `cat port_status.txt | grep Up | cut -d ":" -f 2| cut -d " " -f 2` 
-               echo "[*] All the Working IP's stored in ./Working_ips.txt"
-               a=`cat Working_ips.txt`
-               while read -r line;do 
+               echo ` nmap -p 80,443 -Pn -iL ip.txt` > port_status.txt
+               echo `cat port_status.txt | awk '{ print $16 } '| tr -d '()'` > active_ip.txt
+               echo "[*] All the active IP's are generated in ./active_ip.txt"
+               while read -r line;do
                		`host $line | cut -d " " -f 5` > b
                         echo "[~] $b is active"
-               done < "$a"
+               done < "active_ip.txt"
         else
                echo "[*] Checking Whether Domain working or not !!"
-               echo `nslookup $1 | awk '/^Address: / { print $2 }'` > y.txt 
-               echo `nmap -p 80,443 -sX -Pn -iL y.txt` > domain_status.txt
-               echo `cat domain_status.txt | grep Up | cut -d ":" -f 2| cut -d " " -f 2` > Working_domain.txt
-               z=`cat Working_domain.txt` 
+               echo `nslookup $1 | awk '/^Address: / { print $2 }'` > y.txt
+               echo ` nmap -p 80,443 -sX -Pn -iL y.txt` > domain_status.txt
+               z=`cat domain_status.txt | awk '{ print $16 } '| tr -d '()'` 
                if [ "$z" != "" ];then
               		 echo "[!] $1 is active "
                else
