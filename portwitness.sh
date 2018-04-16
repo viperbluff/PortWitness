28005b5e92cf92b459ad5db26599cf487fe97d80
smartengineers <pig.wig45@gmail.com>
2018-04-16 08:55:37 +0530

    Changes in the banner and loops

--- portwitness.sh
+++ portwitness.sh
@@ -1,32 +1,48 @@
 #!/bin/bash
 tput clear
 if [ "$1" == "" ]; then
-tput bold
-echo -e  "\033[32m ###                PortWitness      ###"
-echo -e  "\033[34m ###                OSINT @tikoo_sahil ###\n"
-echo     "[*]Specify a Domain name"
+	tput bold
+	echo -e  "\033[32m ###                PortWitness               ###"
+	echo -e  "\033[32m ###              OSINT @tikoo_sahil          ###\n"
+	echo -e  "\033[34m [!]Usage: ./portwitness.sh url"
 else 
         tput bold
-        echo -e  "\033[32m ###                PortWitness      ###"
-        echo -e  "\033[34m ###                OSINT @tikoo_sahil ###\n"
-	echo "[*] Want to check for the sub-domain enum(Y,N)?"
+        echo -e  "\033[32m ###                PortWitness               ###"
+        echo -e  "\033[32m ###                OSINT @tikoo_sahil        ###\n"
+	echo -e  "\033[34m [!] Want to check for the sub-domain enum(Y,N)?"
         read enum
         if [ "$enum" == "Y" ] || [ "$enum" == "y" ]; then
 	       cd Sublist3r
                echo "Searching For Sub-Domains ........"
                m=`./sublist3r.py -d $1 -o output.txt`
-               while read -r line; do
-                        echo `nslookup $line | awk '/^Address: / { print $2 }'` >> ip.txt
-               done < "output.txt"
-               echo "[*] IP's for all sub-domains generated in ./Sublist3r/ip.txt"
-               l=`nmap -p 80,443 -Pn -iL ip.txt -oG port_status.txt`
-               echo `cat port_status.txt | grep Up | cut -d ":" -f 2 | cut -d " " -f 2` > active_ip.txt
-               echo `awk ' { for(i=1;i<=NF;i++) { print $i } } ' active_ip.txt` > active_ip[1].txt
-               echo "[*] All the active IP's are generated in ./Sublist3r/active_ip[1].txt"
+               if [ -f ip_$1.txt ]; then
+                        rm ip_$1.txt 
+               		while read -r line; do
+                        	echo `nslookup $line | awk '/^Address: / { print $2 }'` >> ip_$1.txt
+               		done < "output.txt"
+               else
+               		while read -r line; do
+                        	echo `nslookup $line | awk '/^Address: / { print $2 }'` >> ip_$1.txt
+               		done < "output.txt"
+               fi
+	       echo "[*] IP's for all sub-domains generated in ./Sublist3r/ip_$1.txt"
+               if [ -f port_status_$1.txt ]; then
+                        rm port_status_$1.txt
+               		l=`nmap -p 80,443 -Pn -iL ip_$1.txt -oG port_status_$1.txt`
+               else
+                        l=`nmap -p 80,443 -Pn  -iL ip_$1.txt -oG port_status_$1.txt`
+               fi 
+               if [ -f active_ip_$1.txt ]; then
+               		rm active_ip_$1.txt
+               		echo `cat port_status_$1.txt | grep Up | cut -d ":" -f 2 | cut -d " " -f 2` > active_ip_$1.txt
+               else 
+                   	echo `cat port_status_$1.txt | grep Up | cut -d ":" -f 2 | cut -d " " -f 2` > active_ip_$1.txt
+               fi
+               echo "[*] All the active IP's are generated in ./Sublist3r/active_ip_$1.txt"
                while read -r line;do
                		b=`host $line | cut -d " " -f 5`
                         echo "[~] $b is active"
-               done < "active_ip[1].txt"
+               done < "active_ip_$1.txt"
         else
                echo "[*] Checking Whether Domain working or not !!"
                echo `nslookup $1 | awk '/^Address: / { print $2 }'` > y.txt
