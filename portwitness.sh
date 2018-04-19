07319099460675f0c628e3ca72a03e3136b4a764
smartengineers <pig.wig45@gmail.com>
2018-04-19 06:38:54 +0530

    Made changes in color,fetching active results.

--- portwitness.sh
+++ portwitness.sh
@@ -4,16 +4,17 @@ if [ "$1" == "" ]; then
 	tput bold
 	echo -e  "\033[32m ###                PortWitness               ###"
 	echo -e  "\033[32m ###              OSINT @tikoo_sahil          ###\n"
-	echo -e  "\033[34m [!]Usage: ./portwitness.sh url"
+	echo -e  "\033[34m [!]Usage: ./portwitness.sh url\e[0m"
 else 
         tput bold
         echo -e  "\033[32m ###                PortWitness               ###"
         echo -e  "\033[32m ###                OSINT @tikoo_sahil        ###\n"
-	echo -e  "\033[34m [!] Want to check for the sub-domain enum(Y,N)?"
+	echo -e  "\033[34m[!]Check for Active Sub-Domains(Y,N)?\e[0m"
         read enum
         if [ "$enum" == "Y" ] || [ "$enum" == "y" ]; then
 	       cd Sublist3r
-               echo "Searching For Sub-Domains ........"
+               echo -e "\n"
+               echo -e "\e[1m\033[31m[!]\e[0mSearching For Sub-Domains ........\e[0m\n"
                m=`./sublist3r.py -d $1 -o output.txt`
                if [ -f ip_$1.txt ]; then
                         rm ip_$1.txt 
@@ -25,7 +26,7 @@ else
                         	echo `nslookup $line | awk '/^Address: / { print $2 }'` >> ip_$1.txt
                		done < "output.txt"
                fi
-	       echo "[*] IP's for all sub-domains generated in ./Sublist3r/ip_$1.txt"
+	       echo -e "\e[1m\e[34m[!]\e[0m IP's for all sub-domains generated in ./Sublist3r/ip_$1.txt\e[0m"
                if [ -f port_status_$1.txt ]; then
                         rm port_status_$1.txt
                		l=`nmap -p 80,443 -Pn -iL ip_$1.txt -oG port_status_$1.txt`
@@ -38,20 +39,57 @@ else
                else 
                    	echo `cat port_status_$1.txt | grep Up | cut -d ":" -f 2 | cut -d " " -f 2` > active_ip_$1.txt
                fi
-               echo "[*] All the active IP's are generated in ./Sublist3r/active_ip_$1.txt"
-               while read -r line;do
-               		b=`host $line | cut -d " " -f 5`
-                        echo "[~] $b is active"
-               done < "active_ip_$1.txt"
+               if [ -f result.txt ]; then
+               		rm result.txt
+                        a=`awk ' { for(i=1;i<=NF;i++) { print $i } } ' active_ip_$1.txt| tee -a result.txt`
+	       else
+               		a=`awk ' { for(i=1;i<=NF;i++) { print $i } } ' active_ip_$1.txt| tee -a result.txt`
+               fi
+               echo -e "\e[1m\e[34m[!]\e[0m All the active IP's are generated in ./Sublist3r/result.txt\e[0m\n" 
+               echo -e "\e[1m\e[34m[!]\e[0mThe Following sub-domains are Scanned:\e[0m"
+               if [ -f active_domain.txt ]; then
+               		rm active_domain.txt 
+               		while read -r line;do
+                        	r=`nslookup $line | awk '/^Address: / { print $2 }'`
+                        	if [ "$r" == "" ];then
+                        		echo -e "\e[1m\e[31m[!]\e[0m\e[0m$line is \e[31minactive\e[0m"
+                        	else
+                        		file_exist=$(cat result.txt | grep -c "$r")
+                        		if [ $file_exist -eq 0 ]; then  
+                        			echo -e "\e[1m\e[31m[~]\e[0m\e[0m $line is \e[31minactive\e[0m"
+                        		else 
+                                        	echo -e "\e[1m\e[34m[!]\e[0m\e[0m$line is \e[32mactive\e[0m" 
+                                        	echo -e "\e[1m\e[34m[!]\e[0m\e[0m$line is \e[32mactive\e[0m" >> active_domain.txt
+                                        fi
+                                fi
+                        done < "output.txt"
+                else
+                	while read -r line;do
+                        r=`nslookup $line | awk '/^Address: / { print $2 }'`
+                        if [ "$r" == "" ];then
+                                echo -e "\e[1m\e[31m[!]\e[0m\e[0m$line is \e[31minactive\e[0m"
+                        else
+                                file_exist=$(cat result.txt | grep -c "$r")
+                                if [ $file_exist -eq 0 ]; then  
+                                        echo -e "\e[1m\e[31m[~]\e[0m\e[0m $line is \e[31minactive\e[0m"
+                                else 
+                                        echo -e "\e[1m\e[34m[!]\e[0m\e[0m$line is \e[32mactive\e[0m" 
+                                        echo -e "\e[1m\e[34m[!]\e[0m\e[0m$line is \e[32mactive\e[0m" >> active_domain.txt
+  
+                                fi 
+                        fi
+                        done < "output.txt"
+                fi
+               echo -e "\e[1m\e[35m[!]\e[0mList of all the active sub-domains is stored under ./Sublist3r/active_domain.txt\e[0m" 
         else
-               echo "[*] Checking Whether Domain working or not !!"
+               echo -e "\e[1m\e[32m[*]\e[0m Checking Whether Domain working or not !!"
                echo `nslookup $1 | awk '/^Address: / { print $2 }'` > y.txt
-               o=`nmap -p 80,443 -sX -Pn -iL y.txt -oG domain_status.txt`
+               o=`nmap -p 80,443 -A -sX -Pn -iL y.txt -oG domain_status.txt`
                z=`cat domain_status.txt | grep Up | cut -d ":" -f 2 | cut -d " " -f 2` 
                if [ "$z" != "" ];then
-              		 echo "[!] $1 is active "
+              		 echo "\e[34m[!]\e[0m $1 is active "
                else
-               		echo "[!] $1  is down"
+               		echo "\e[31m[!]\e[0m $1  is down"
                fi
    	 fi
 fi
