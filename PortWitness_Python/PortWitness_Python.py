8b75187d49b89d220d6ac6177d008d9e37c73f13
root <root@localhost.localdomain>
2019-01-21 05:57:08 +0530

    Updated Portwitness Python code, added active.txt

--- PortWitness_Python/PortWitness_Python.py
+++ PortWitness_Python/PortWitness_Python.py
@@ -9,12 +9,15 @@ import subprocess
 
 def Sublist3r(domain):
 	dom=str(domain) 
-	out=subprocess.Popen(['./sublist3r.py' ,'-d',domain,'-o','test3.txt'],stdout=subprocess.PIPE,stderr=subprocess.PIPE,shell=False)
+	print  "\n[*]Sub-Domain Enumeration Process has been started.........."
+	out=subprocess.Popen(['./sublist3r.py' ,'-d',domain,'-o','test_1.txt'],stdout=subprocess.PIPE,stderr=subprocess.PIPE,shell=False)
 	output,error=out.communicate()
+	print "[*]Printing Active/Inactive Sub-Domains..........\n"
 	connection_Test()
 
 def connection_Test():
-	file2=open("test3.txt",'r')
+	file2=open("test_1.txt",'r')
+	file3=open("active.txt",'w')
 	file_list=list(file2)
 	a=[]
 	for i in file_list:
@@ -29,17 +32,21 @@ def connection_Test():
 		try:
 			obj.connect((s,80))
 			print ">> Connected to sub-domain \033[32m %s \033[0m on 80\n" %(j)
+			file3.write(j+":80"+"\n")
+			
 		except Exception as ex:
 			print "[x] Sub-domain %s Inactive on port 80\n" %(j)
 
 		try:
 			obj.connect((s,443))
 			print ">> Connected to sub-domain \033[32m %s \033[0m on 443\n" %(j)
+			file3.write(j++":443"+"\n")
 		except Exception as ex:
 			print "[x] Sub-domain %s Inactive on port 443\n" %(j)
 			continue
-			
+	print "\n[*]All the active sub-domains are stored in active.txt file....\n"
 	file2.close()
+	file3.close()
 	obj.close()
 
 def main():
